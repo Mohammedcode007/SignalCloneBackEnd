d89bf343c754502b861a6ff367f167b471e8cc0f
mohammedcode007 <code.hassan.1992@gmail.com>
2023-07-18 20:32:04 +0300

    kk

--- components/UserItem/UserItem.tsx
+++ components/UserItem/UserItem.tsx
@@ -17,54 +17,35 @@ const UserItem: React.FC<UserItemProps> = ({ oneUserItem }: UserItemProps) => {
   const navigation = useNavigation();
 
   const createChatRoom = async () => {
-    const newChatRoom = await DataStore.save(new ChatRoom({newMessages:0}));
-
-    // TODO if there is already a chat room between these 2 users
-    // then redirect to the existing chat room
-    // otherwise, create a new chatroom with these users.
-
-    // connect authenticated user with the chat room
-    const authUser = await Auth.currentAuthenticatedUser();
-    console.log(authUser.attributes.sub);
-    
-    const dbUser = await DataStore.query(User, authUser.attributes.sub);
-
-    await DataStore.save(new ChatRoomUser({
-      user : dbUser,
-      chatroom : newChatRoom
-    }))
-
-    await DataStore.save(new ChatRoomUser({
-      user : oneUserItem,
-      chatroom : newChatRoom
-    }))
-    // if (!dbUser) {
-    //   Alert.alert("There was an error creating the group");
-    //   return;
-    // }
-    // Create a chat room
-    // const newChatRoomData = {
-    //   newMessages: 0,
-    //   Admin: dbUser,
-    // };
-    // if (users.length > 1) {
-    //   newChatRoomData.name = "New group 2";
-    //   newChatRoomData.imageUri =
-    //     "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/group.jpeg";
-    // }
-
-    // if (dbUser) {
-    //   await addUserToChatRoom(dbUser, newChatRoom);
-    // }
-
-    // connect users user with the chat room
-    // await Promise.all(
-    //   users.map((user) => addUserToChatRoom(user, newChatRoom))
-    // );
-
-    // navigation.navigate('ChatRoomScreen', { chatRoomDataItem: item });
+    try {
+      const newChatRoom = await DataStore.save(new ChatRoom({ newMessages: 0 }));
+  
+      const authUser = await Auth.currentAuthenticatedUser();
+      console.log(authUser.attributes.sub);
+  
+      const loggedInUserId = authUser.attributes.sub;
+      const loggedInUser = await DataStore.query(User, loggedInUserId);
+      const selectedUser = await DataStore.query(User, oneUserItem.id);
+  
+      if (loggedInUser && selectedUser) {
+        await DataStore.save(new ChatRoomUser({
+          user: loggedInUser,
+          chatRoom: newChatRoom
+        }));
+  
+        await DataStore.save(new ChatRoomUser({
+          user: selectedUser,
+          chatRoom: newChatRoom
+        }));
+      }
+  
+      // Rest of the code..
+    } catch (error) {
+      console.error('Error creating chatroom:', error);
+    }
   };
-
+  
+  
  
 
   
